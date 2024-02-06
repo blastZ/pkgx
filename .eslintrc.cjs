26333e9ca08207f9f38d2f3bace812fa9227a0c8
blastz <touchzh1996@outlook.com>
2024-02-06 17:45:22 +0800

    build(repo): moved from pkg universe

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin', 'eslint-plugin-import'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.*', '**/dist/**', 'tools/**', '**/output/**'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'warn',
+      {
+        'newlines-between': 'always',
+      },
+    ],
+    '@typescript-eslint/no-duplicate-enum-values': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-var-requires': 'warn',
+  },
+};
---
+++ README.md
@@ -0,0 +1,7 @@
+# Pkgx
+
+Pkgx is a zero configuration tool for any repository. It support build, test, serve and more.
+
+## License
+
+MIT
