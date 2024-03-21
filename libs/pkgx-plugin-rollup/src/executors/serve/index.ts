871655ea2cd5df021da0cb35d9f6cbb1ff7f82c1
blastz <touchzh1996@outlook.com>
2024-03-21 11:17:57 +0800

    refactor(rollup): better structure

--- libs/pkgx-plugin-rollup/src/executors/serve/index.ts
+++ libs/pkgx-plugin-rollup/src/executors/serve/index.ts
@@ -16,10 +16,10 @@ import {
   PkgxOptions,
 } from '@libs/pkgx-plugin-devkit';
 
-import { getRollupOptions } from '../../utils/get-rollup-options.js';
-import { handleError } from '../../utils/handle-error.js';
-import { logger } from '../../utils/logger.util.js';
-import { relativeId } from '../../utils/relative-id.js';
+import { getRollupOptions } from '../../core/get-rollup-options.js';
+import { handleError } from '../../core/handle-error.js';
+import { logger } from '../../core/logger.util.js';
+import { relativeId } from '../../core/relative-id.js';
 
 /**
  * Executor: @pkgx/rollup:serve
