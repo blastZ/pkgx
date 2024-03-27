c5dddcab2055bb6556d981419a2871c8a0957d81
blastz <touchzh1996@outlook.com>
2024-03-27 15:44:52 +0800

    test(pkgx): update tests

--- tests/pkgx.spec.ts
+++ tests/pkgx.spec.ts
@@ -4,6 +4,34 @@ import { resolve } from 'node:path';
 import request from 'supertest';
 import { $, fs } from 'zx';
 
+async function readFileToString(projectDir: string, filePath: string) {
+  const str = (await fs.readFile(resolve(projectDir, filePath))).toString();
+
+  return str;
+}
+
+async function readFileToJson(projectDir: string, filePath: string) {
+  return JSON.parse(await readFileToString(projectDir, filePath));
+}
+
+async function importPackage(outputDir: string) {
+  return import(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    outputDir
+  );
+}
+
+function requirePackage(outputDir: string) {
+  const require = createRequire(import.meta.url);
+
+  return require(
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    outputDir,
+  );
+}
+
 describe('pkgx', () => {
   // beforeAll(async () => {
   //   await $`./scripts/build.js`;
@@ -14,80 +42,81 @@ describe('pkgx', () => {
 
     await $`pkgx build ${dir}`;
 
-    const { maskStr, currentYear } = await import(
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      './projects/node-package/output'
+    const { maskStr, currentYear } = await importPackage(
+      './projects/node-package/output',
     );
 
     expect(maskStr('123456', 1, 4)).toBe('1****6');
     expect(currentYear()).toBe(new Date().getFullYear().toString());
 
     expect(
-      (
-        await fs.readFile(resolve(dir, './output/src/assets/temp.txt'))
-      ).toString(),
-    ).toEqual('temp');
+      (await readFileToString(dir, './output/index.d.ts')).split('\n'),
+    ).toEqual([
+      'declare function maskStr(str: string, start: number, count: number): string;',
+      'declare function currentYear(): string;',
+      '',
+      'export { currentYear, maskStr };',
+      '',
+    ]);
+
+    expect(await readFileToString(dir, './output/src/assets/temp.txt')).toEqual(
+      'temp',
+    );
     expect(
-      (
-        await fs.readFile(resolve(dir, './output/src/assets/views/home.ejs'))
-      ).toString(),
+      await readFileToString(dir, './output/src/assets/views/home.ejs'),
     ).toEqual('<p>home</p>');
 
-    const pkgJson = JSON.parse(
-      (await fs.readFile(resolve(dir, './output/package.json'))).toString(),
-    );
-
-    expect(pkgJson.name).toEqual('node-package');
-    expect(pkgJson.type).toEqual('module');
-    expect(pkgJson.main).toEqual('./cjs/index.js');
-    expect(pkgJson.exports).toEqual({
+    expect(await readFileToJson(dir, './output/package.json')).toEqual({
+      name: 'node-package',
+      type: 'module',
+      main: './cjs/index.js',
+      exports: {
+        types: './index.d.ts',
+        require: './cjs/index.js',
+        import: './esm/index.js',
+      },
       types: './index.d.ts',
-      require: './cjs/index.js',
-      import: './esm/index.js',
+      dependencies: {
+        dayjs: '^1.11.10',
+      },
+      version: '1.5.2',
+      description: '',
+      homepage: '',
+      author: '',
+      license: 'ISC',
     });
-    expect(pkgJson.types).toEqual('./index.d.ts');
-    expect(pkgJson.dependencies).toEqual({
-      dayjs: '^1.11.10',
+
+    expect(await readFileToJson(dir, './output/cjs/package.json')).toEqual({
+      name: 'node-package',
+      type: 'commonjs',
     });
-    expect(pkgJson.version).toEqual('1.5.2');
-
-    const cjsPkgJson = JSON.parse(
-      (await fs.readFile(resolve(dir, './output/cjs/package.json'))).toString(),
-    );
-
-    expect(cjsPkgJson.name).toEqual('node-package');
-    expect(cjsPkgJson.type).toEqual('commonjs');
   }, 5000);
 
   it('should build esm app', async () => {
     const dir = 'tests/projects/node-app';
 
     const checkBuildResult = async () => {
-      const pkgJson = JSON.parse(
-        (await fs.readFile(resolve(dir, './output/package.json'))).toString(),
-      );
-
-      expect(pkgJson.name).toEqual('node-app');
-      expect(pkgJson.type).toEqual('module');
-      expect(pkgJson.main).toEqual('./esm/index.js');
-      expect(pkgJson.types).toEqual('./index.d.ts');
-      expect(pkgJson.dependencies).toEqual({
-        chalk: '^5.3.0',
-        express: '^4.18.2',
+      expect(await readFileToJson(dir, './output/package.json')).toEqual({
+        name: 'node-app',
+        type: 'module',
+        main: './esm/index.js',
+        types: './index.d.ts',
+        dependencies: {
+          chalk: '^5.3.0',
+          express: '^4.18.2',
+        },
+        scripts: {
+          start: 'node esm/index.js',
+          test: 'echo test',
+        },
+        version: '1.3.1',
+        description: '',
+        homepage: '',
+        author: '',
+        license: 'ISC',
       });
-      expect(pkgJson.devDependencies).toBeUndefined();
-      expect(pkgJson.version).toEqual('1.3.1');
-      expect(pkgJson.scripts).toEqual({
-        start: 'node esm/index.js',
-        test: 'echo test',
-      });
 
-      const { app } = await import(
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        './projects/node-app/output'
-      );
+      const { app } = await importPackage('./projects/node-app/output');
 
       expect(app.listen).toBeDefined();
     };
@@ -103,31 +132,28 @@ describe('pkgx', () => {
     const dir = 'tests/projects/node-cjs-app';
 
     const checkBuildResult = async () => {
-      const pkgJson = JSON.parse(
-        (await fs.readFile(resolve(dir, './output/package.json'))).toString(),
-      );
+      expect(await readFileToJson(dir, './output/package.json')).toEqual({
+        name: 'node-cjs-app',
+        main: './cjs/index.js',
+        types: './index.d.ts',
+        dependencies: {
+          fastify: '^4.26.2',
+        },
+        scripts: {
+          start: 'node cjs/index.js',
+        },
+        version: '2.2.2',
+        description: '',
+        homepage: '',
+        author: '',
+        license: 'ISC',
+      });
 
-      expect(pkgJson.name).toEqual('node-cjs-app');
-      expect(pkgJson.type).toBeUndefined();
-      expect(pkgJson.main).toEqual('./cjs/index.js');
-      expect(pkgJson.types).toEqual('./index.d.ts');
-      expect(pkgJson.dependencies).toEqual({
-        fastify: '^4.26.2',
-      });
-      expect(pkgJson.version).toEqual('2.2.2');
-      expect(pkgJson.scripts).toEqual({
-        start: 'node cjs/index.js',
-      });
       expect(
         await fs.exists(resolve(dir, './output/cjs/package.json')),
       ).toEqual(false);
 
-      const require = createRequire(import.meta.url);
-      const {
-        app,
-      } = require(// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      './projects/node-cjs-app/output');
+      const { app } = requirePackage('./projects/node-cjs-app/output');
 
       expect(app.listen).toBeDefined();
     };
@@ -143,12 +169,7 @@ describe('pkgx', () => {
     const dir = 'tests/projects/nest-cjs-app';
 
     const checkBuildResult = async () => {
-      const require = createRequire(import.meta.url);
-      const {
-        getServer,
-      } = require(// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      './projects/nest-cjs-app/output');
+      const { getServer } = requirePackage('./projects/nest-cjs-app/output');
 
       const server = await getServer();
 
@@ -169,10 +190,8 @@ describe('pkgx', () => {
     const dir = 'tests/projects/nest-esm-app';
 
     const checkBuildResult = async () => {
-      const { getServer } = await import(
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        './projects/nest-esm-app/output'
+      const { getServer } = await importPackage(
+        './projects/nest-esm-app/output',
       );
 
       const server = await getServer();
@@ -195,19 +214,20 @@ describe('pkgx', () => {
 
     await $`pkgx build ${dir}`;
 
-    const { echo } = await import(
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      './projects/simplest/output'
-    );
+    const { echo } = await importPackage('./projects/simplest/output');
 
     expect(echo('123')).toBe('123');
 
-    const pkgJson = JSON.parse(
-      (await fs.readFile(resolve(dir, './output/package.json'))).toString(),
-    );
-
-    expect(pkgJson.name).toEqual('simplest');
+    expect(await readFileToJson(dir, './output/package.json')).toEqual({
+      name: 'simplest',
+      main: './cjs/index.js',
+      types: './index.d.ts',
+      version: '1.0.0',
+      description: '',
+      homepage: '',
+      author: '',
+      license: 'ISC',
+    });
   }, 5000);
 
   it('should work with config generator', async () => {
